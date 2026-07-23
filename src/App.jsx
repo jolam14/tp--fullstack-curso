f146cda1715734f6c740c82ef4171c5d129ccaaa
jolam14 <joselameda14@gmail.com>
2026-07-23 17:55:53 -0300

    Add character limit and word/sentence counting
    Implement character limiting feature and add word and sentence counters to the text analyzer. Includes state management for limit toggle and limit value, plus event handlers for textarea and limit input changes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
+import { useState } from "react"
 import { Header } from "./components/Header.jsx"
 
 const App = () => {
-  const [text, setText] = userState("esto es texto de prueba se puede borrar")
+  const [text, setText] = useState("esto es texto de prueba se puede borrar")
   const [excludeSpace, setExcludeSpace] = useState(false)
 
+  
+  const [limitCharacter, setlimiteCharacter] =userState(false)
+  const [limitValue, setLimitValue] = useState(300)
   const Character = setExcludeSpace ? text.replace(/\s/g,"").length: text.length 
-  const [limitCharacter]
-}
+  const words = text.trim() === "" ? 0 : text.trim().split(/\s+/).length
+  const senteces = text.trim() === "" ? 0 : text.split(/[.!?]+/).filter(senteces => senteces.trim() !== "").length
 
+  const handleChangeTextarea = (e) => {
+    const value = e.target.value
+
+    if (limitCharacter) {
+      if (e.target.value <= limitValue) {
+        setText(e.target.value)
+      }
+    } else [
+      setText(e.target.value)
+    ]
+  }
+  const handleChangeInputLimit = () => {
+    setlimiteCharacter(!limitCharacter)
+    const newText = text.slice(0, limitValue)
+    setText(value)
+  }
+}
   
 function App() {
   return <h1></h1>
@@ -23,20 +44,25 @@ function App() {
       <div>
         <label >
           <input 
-          type="checkbox"
-          checked={excludeSpace}
-          onchance {(e) => setExcludeSpace(!excludeSpace)}
+          type = "checkbox"
+          checked = {excludeSpace}
+          onchance = {(e) => setExcludeSpace(!excludeSpace)}
           />
           exluir espacios
         </label>
           <label >
           <input 
-          type="checkbox"
-          checked={limitCharacter}
-          onchance {(e) => setlimiteCharacter(!limitCharacter)}
+          type = "checkbox"
+          checked = {limitCharacter}
+          onchance = {(e) => setlimiteCharacter(!limitCharacter)}
           />
           limite de Caracteres
         </label>
+        {
+        limitCharacter && <input 
+        type="number"
+        aria-valuemax={()}
+         /> }
       </div>
     </main>
   )
